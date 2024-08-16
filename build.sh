a8b3bf2780d5ad292d9801e07ad3ed451fc6386b
Dan VIAL <dan.vial@dbmail.com>
2024-08-16 19:12:31 +0200

    16/08/2024 19:12:31

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+logMsg() {
+  printf "%s\n" "`date`: ${1}" >> "${logNameFile}"
+}
+
 echo "***** dist ci test *****"
+
+logNameFile="log-ci"
 
 dist="dist"
 
-mkdir "${dist}"
-cp "test1" "${dist}/test1"
-cp "test2" "${dist}/test2"
+if [ ! -d "$dist" ]; then
+  logMsg "create directory ${dist}"
+  mkdir "${dist}"
+fi
 
+logMsg "copi files code1"
+cp "code1" "${dist}/code1"
+
+logMsg "copi files code2"
+cp "code2" "${dist}/code2"
