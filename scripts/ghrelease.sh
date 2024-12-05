5483991df11d3043c3325ec96aebcd6903cea906
Dan VIAL <dan.vial@dbmail.com>
2024-12-05 16:06:27 +0100

    test: 05/12/2024 16:06:27

--- scripts/ghrelease.sh
+++ scripts/ghrelease.sh
@@ -2,4 +2,11 @@
 set -e
 
 echo "Starting create release with TAG..." >> log-ci.log
-gh release create "$2" "./$1-$2-dist.tar.gz" --target "$3" >> log-ci.log
+gh release create "$2" "./${1}-${2}-dist.tar.gz" --target "$3" >> log-ci.log
+
+# Envoyer un événement webhook personnalisé
+curl -X POST \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{"event_type":"release_created","client_payload":{"tag":"'"$2"'"}}' \
+  "https://api.github.com/repos/${4}/dispatches" >> log-ci.log
